02e289fac7400f0925ce74620ee37a59f63e35d8
manikandan <manidhiyamech07167@gmail.com>
2022-08-27 20:22:56 +0530

    alphabet only allowed

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function mycity(){
+	var  str=document.getElementById("city").value;
+	var cityAlert=document.getElementById("city-alert");
+	if(!((/[a-zA-Z]/.test(str))|| str.length==0)){
+		cityAlert.style.display="block";
+		cityAlert.innerHTML="only Alphabets are allowed";
+	}
+	else{
+		cityAlert.style.display="none";
+	}
+}
+
+function mynumber(){
+	var  str1=document.getElementById("num").value;
+	var numAlert=document.getElementById("num-alert");
+	if(!((/[0-9]/.test(str1))|| str1.length==0)){
+		numAlert.style.display="block";
+		numAlert.innerHTML="only Numbers are allowed";
+	}
+	else{
+		numAlert.style.display="none";
+	}
+}
+
+
+var str="m2ani2345";
+var x = /[0-9]/;
+
+var sum=0;
+
+for(i=0;i<str.length;i++){
+	if(x.test(str[i])){
+	sum+=parseInt(str[i]);
+}
+}
+console.log(sum);
